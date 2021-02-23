5ee6d2f992e739d676d06a266b5739b4386a4ad4
kcenon <kcenon@naver.com>
2021-02-23 17:15:49 +0900

    Appended rule of file handling related to log

--- logging/logging.h
+++ logging/logging.h
@@ -2,6 +2,7 @@
 
 #include "logging_level.h"
 
+#include <queue>
 #include <mutex>
 #include <memory>
 #include <atomic>
@@ -21,36 +22,58 @@ namespace logging
 		~util(void);
 
 	public:
-		bool start(void);
+		bool start(const std::wstring& store_log_file_name = L"log", const std::wstring& store_log_extention = L"log", const std::wstring& store_log_root_path = L"");
 		bool stop(void);
 
 	public:
 		void set_target_level(const logging_level& target_level);
 		void set_write_console(const bool& write_console);
+		void set_store_latest_log_count(const size_t& store_latest_log_count);
+		void set_limit_log_file_size(const size_t& limit_log_file_size);
+
+	public:
+		const std::queue<std::wstring> get_latest_logs(void);
 
 	public:
 		std::chrono::time_point<std::chrono::steady_clock> chrono_start(void);
 		void write(const logging_level& target_level, const std::wstring& log_data);
 		void write(const logging_level& target_level, const std::wstring& log_data, const std::chrono::time_point<std::chrono::steady_clock>& time);
 
-	private:
+	protected:
 		void run(void);
+
+	private:
+		void start_log(void);
+		void backup_log(const std::wstring& target_path, const std::wstring& backup_path);
+		void store_log(int& file_handle, const std::wstring& log);
+		void end_log(void);
+
+	private:
+		std::vector<unsigned char> load(const std::wstring& path);
+		void append(const std::wstring& source, const std::wstring& target);
 
 	private:
 		std::vector<std::pair<logging_level, std::pair<std::chrono::system_clock::time_point, std::wstring>>> _buffer;
 
 	private:
 		logging_level _target_level;
+		std::wstring _store_log_root_path;
+		std::wstring _store_log_file_name;
+		std::wstring _store_log_extention;
 
 	private:
 		std::atomic<bool> _has_buffer{ false };
 		std::atomic<bool> _transfer_buffer{ false };
 		std::atomic<bool> _thread_stop{ false };
+		std::atomic<bool> _write_date{ false };
 		std::atomic<bool> _write_console{ true };
 		std::atomic<bool> _write_file{ true };
+		std::atomic<size_t> _store_latest_log_count{ 1000 };
+		std::atomic<size_t> _limit_log_file_size{ 2097152 };
 
 	private:
 		std::thread _thread;
+		std::queue<std::wstring> _latest_logs;
 
 #pragma region singleton
 	public:
