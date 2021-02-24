f6a3a3a7ff9f1b22a2f8f5dd7fea5deb628e12c7
kcenon <kcenon@naver.com>
2021-02-25 00:35:31 +0900

    Appended thread safe code for logging

--- logging_sample/logging_sample.cpp
+++ logging_sample/logging_sample.cpp
@@ -1,4 +1,4 @@
-#include "logging.h"
+﻿#include "logging.h"
 
 #include "limits.h"
 
@@ -9,11 +9,26 @@
 int main()
 {
 	logging::util::handle().start();
-	for (unsigned int index = 0; index < UINT_MAX; ++index)
+
+	std::vector<std::thread> threads;
+	for (unsigned short index = 0; index < 10; ++index)
 	{
-		logging::util::handle().write(logging::logging_level::information, fmt::format(L"test_{}", index));
-		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+		threads.push_back(
+			std::thread([]
+				{
+					for (unsigned int index = 0; index < 1000; ++index)
+					{
+						logging::util::handle().write(logging::logging_level::information, fmt::format(L"테스트_in_thread_{}", index));
+					}
+				})
+		);
 	}
+
+	for (auto& thread : threads)
+	{
+		thread.join();
+	}
+
 	logging::util::handle().stop();
 
     return 0;
