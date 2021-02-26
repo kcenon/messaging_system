f96761207051719bc0ee3fb18520bf11d9fb0e98
raphael.shin <raphael.shin@raymedical.co.kr>
2021-02-26 16:14:09 +0900

    Changed project name from logging to utilities

---
+++ utilities/converting.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <locale>
+#include <vector>
+#include <string>
+
+namespace converting
+{
+	class util
+	{
+	public:
+		static std::wstring to_wstring(const std::string& value);
+		static std::string to_string(const std::wstring& value);
+
+	public:
+		static std::vector<char> to_array(const std::wstring& value);
+		static std::wstring to_wstring(const std::vector<char>& value);
+	};
+};
+
