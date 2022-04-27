f82051034b87c8013d042e0b8c4efb9a06304f52
kcenon <kcenon@naver.com>
2022-04-27 13:52:15 +0000

    Appended 'dependency_install.sh' to support installing of dependency libraries

---
+++ dependency_install.sh
@@ -0,0 +1,23 @@
+echo ""
+
+apt update
+apt upgrade -y
+
+apt install cmake build-essential gdb -y
+
+apt-get update
+apt-get upgrade -y
+
+apt-get install curl zip unzip tar ninja-build -y
+apt-get install pkg-config autoconf -y
+
+cd ..
+
+if [ ! -d "./vcpkg/" ]
+then
+    git clone https://github.com/microsoft/vcpkg.git
+fi
+cd vcpkg
+git pull
+./bootstrap-vcpkg.sh
+./vcpkg install lz4 fmt cpprestsdk cryptopp asio python3 crossguid
