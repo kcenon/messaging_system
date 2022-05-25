efacded438e3e61ce0d93ad6d5c96c3a1bd58ace
kcenon <kcenon@naver.com>
2022-05-25 16:50:03 +0900

    Implementing unittest on utilities library

--- unittest/utilities.cpp
+++ unittest/utilities.cpp
@@ -2,142 +2,93 @@
 
 #include "gtest/gtest.h"
 
-TEST(argument, parse)
+#include <string>
+#include <vector>
+
+#include "encrypting.h"
+#include "converting.h"
+#include "compressing.h"
+#include "argument_parser.h"
+
+using namespace std;
+using namespace encrypting;
+using namespace converting;
+using namespace compressing;
+using namespace argument_parser;
+
+TEST(argument, test)
 {
-
-}
-
-TEST(compressor, compression)
-{
-
-}
-
-TEST(compressor, decompression)
-{
+    char* test1[] = {(char*)"test.exe", (char*)"--version", (char*)"1.000"};
+    wchar_t* test2[] = {(wchar_t*)L"test.exe", (wchar_t*)L"--version", (wchar_t*)L"1.000"};
     
+    auto result1 = argument::parse(3, test1);
+    auto result2 = argument::parse(3, test2);
+
+    EXPECT_EQ(result1, result2);
 }
 
-TEST(converter, replace)
+TEST(compressor, test)
+{
+    vector<uint8_t> original;
+    for(int i=0; i<256; ++i)
+    {
+        for(int j=0; j<256; ++j)
+        {
+            original.push_back((uint8_t)j);
+        }
+    }
+
+    vector<uint8_t> compressed = compressor::compression(original);
+
+    EXPECT_TRUE(original != compressed);
+
+    vector<uint8_t> decompressed = compressor::decompression(compressed);
+
+    EXPECT_EQ(original, decompressed);
+}
+
+TEST(converter, test)
+{
+    wstring original = L"Itestamtestatestprogrammer";
+    wstring source = original;
+    wstring token = L"test";
+    wstring target = L" ";
+
+    converter::replace(source, token, target);
+
+    EXPECT_TRUE(source.compare(L"I am a programmer") == 0);
+
+    source = converter::replace2(original, token, target);
+
+    EXPECT_TRUE(source.compare(L"I am a programmer") == 0);
+
+    EXPECT_TRUE(converter::to_string(L"test has passed").compare("test has passed") == 0);
+    EXPECT_TRUE(converter::to_wstring("test has passed").compare(L"test has passed") == 0);
+    EXPECT_TRUE(converter::to_string(converter::to_array("test has passed")).compare("test has passed") == 0);
+    EXPECT_TRUE(converter::to_wstring(converter::to_array(L"test has passed")).compare(L"test has passed") == 0);
+}
+
+TEST(datetime_handler, test)
 {
 
 }
 
-TEST(converter, replace2)
-{
-    
-}
-
-TEST(converter, to_string)
+TEST(encryptor, test)
 {
 
 }
 
-TEST(converter, to_wstring)
+TEST(file, test)
 {
 
 }
 
-TEST(converter, to_arryay)
+TEST(folder, test)
 {
 
 }
 
-TEST(converter, from_base64)
-{
-
-}
-
-TEST(converter, to_base64)
-{
-
-}
-
-TEST(datetime_handler, current_time)
-{
-
-}
-
-TEST(datetime_handler, time)
-{
-
-}
-
-TEST(encryptor, create_key)
-{
-
-}
-
-TEST(encryptor, encryption)
-{
-
-}
-
-TEST(encryptor, decryption)
-{
-
-}
-
-TEST(file, save)
-{
-
-}
-
-TEST(file, append)
-{
-
-}
-
-TEST(file, load)
-{
-
-}
-
-TEST(file, remove)
-{
-
-}
-
-TEST(folder, get_temporary_folder)
-{
-
-}
-
-TEST(folder, create_folder)
-{
-
-}
-
-TEST(folder, delete_folder)
-{
-
-}
-
-TEST(folder, delete_folders)
-{
-
-}
-
-TEST(folder, get_folders)
-{
-
-}
-
-TEST(folder, get_files)
-{
-
-}
-
-TEST(logger, start)
-{
-
-}
-
-TEST(logger, write)
-{
-
-}
-
-TEST(logger, stop)
+TEST(logger, test)
 {
 
 }
