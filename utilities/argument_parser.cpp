33de71f7af1e856aa2ba41067a9694db377047a0
kcenon <kcenon@naver.com>
2022-06-05 05:33:20 +0000

    Refactoring variable's scope

--- utilities/argument_parser.cpp
+++ utilities/argument_parser.cpp
@@ -62,13 +62,12 @@ namespace argument_parser
 	map<wstring, wstring> argument_manager::parse(int argc, char* argv[])
 	{
 		map<wstring, wstring> result;
-
-		size_t offset;
+		
 		wstring argument_id;
 		for (int index = 1; index < argc; ++index)
 		{
 			argument_id = converter::to_wstring(argv[index]);
-			offset = argument_id.find(L"--", 0);
+			size_t offset = argument_id.find(L"--", 0);
 			if (offset != 0)
 			{
 				continue;
@@ -104,13 +103,12 @@ namespace argument_parser
 	map<wstring, wstring> argument_manager::parse(int argc, wchar_t* argv[])
 	{
 		map<wstring, wstring> result;
-
-		size_t offset = 0;
+		
 		wstring argument_id;
 		for (int index = 1; index < argc; ++index)
 		{
 			argument_id = argv[index];
-			offset = argument_id.find(L"--", 0);
+			size_t offset = argument_id.find(L"--", 0);
 			if (offset != 0)
 			{
 				continue;
