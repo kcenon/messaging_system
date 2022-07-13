ea8eafda18a494a8fe69718400baf8d593bf5de1
kcenon <kcenon@naver.com>
2022-07-14 02:07:21 +0900

    Changed build.sh and CMakeLists.txt files to support static & shared build

--- build.sh
+++ build.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+
+rm -rf bin
 rm -rf build
+
 mkdir build
 cd build
-cmake .. -DCMAKE_TOOLCHAIN_FILE="../../vcpkg/scripts/buildsystems/vcpkg.cmake" -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_TOOLCHAIN_FILE="../../vcpkg/scripts/buildsystems/vcpkg.cmake" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=YES
 make -B
 export LC_ALL=C
 unset LANGUAGE
-./unittest/unittest
+
+cd ..
+./bin/unittest
+
+rm -rf build
