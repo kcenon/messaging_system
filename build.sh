f23d5fd8817230fd8e36f35d7fea3ed1c518baaa
kcenon <kcenon@naver.com>
2022-07-13 23:48:35 +0900

    Updated CMakeLists.txt

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 rm -rf build
 mkdir build
 cd build
-cmake .. -DCMAKE_TOOLCHAIN_FILE="../../vcpkg/scripts/buildsystems/vcpkg.cmake"
+cmake .. -DCMAKE_TOOLCHAIN_FILE="../../vcpkg/scripts/buildsystems/vcpkg.cmake" -DCMAKE_BUILD_TYPE=Release
 make -B
 export LC_ALL=C
 unset LANGUAGE
