41183e484ad556aa20d163e7ff8a7581295a1c34
kcenon <kcenon@naver.com>
2022-07-15 14:04:55 +0900

    Removed SWIG interface

--- build.sh
+++ build.sh
@@ -3,16 +3,17 @@
 rm -rf bin
 rm -rf build
 
-./interface.sh
-
 mkdir build
 cd build
-cmake .. -DCMAKE_TOOLCHAIN_FILE="../../vcpkg/scripts/buildsystems/vcpkg.cmake" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=YES -DUSE_SWIG_INTERFACE=ON
+cmake .. -DCMAKE_TOOLCHAIN_FILE="../../vcpkg/scripts/buildsystems/vcpkg.cmake" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=YES
 make -B
 export LC_ALL=C
 unset LANGUAGE
 
 cd ..
-./bin/unittest
+
+if [ ! -f "./bin/unittest" ]; then
+    ./bin/unittest
+fi
 
 rm -rf build
